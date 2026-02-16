96269ea42e9d0d10d8636e326f8236d695a3573d
Killshot0031 <zach.k.h97@gmail.com>
2026-02-16 17:14:18 -0600

    PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
-import NavBar from '../components/NavBar';
+import { useEffect } from "react";
+import "../styles/globals.css";
+import NavBar from "../components/NavBar";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js");
+    }
+  }, []);
+
   return (
     <>
       <NavBar />
